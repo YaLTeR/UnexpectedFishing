fb2d61265fbcd6c93965628e21e0d110f5146bb1
Ivan Molodetskikh <yalterz@gmail.com>
2013-10-10 21:50:53 +0400

    Added GPL v3 notices.

--- src/unexpectedfishing/UnexpectedFishingListener.java
+++ src/unexpectedfishing/UnexpectedFishingListener.java
@@ -1,3 +1,21 @@
+/*
+ *  Copyright 2013 Ivan Molodetskikh.
+ *  
+ *  This file is part of UnexpectedFishing.
+ *
+ *  UnexpectedFishing is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  UnexpectedFishing is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with UnexpectedFishing.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package unexpectedfishing;
 
 import java.util.ArrayList;
